ef2a8fe265ed96245f8e37e5f092d53093f17bd8
subsme <subusundaram@gmail.com>
2024-01-12 16:30:24 -0800

    commit to master

---
+++ capsuleRun/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    bool dead = false;
+
+    private void Update()
+    {
+        if (transform.position.y < -1f && !dead)
+        {
+            Die();
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {   
+        if (collision.gameObject.CompareTag("Enemy Body"))
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<PlayerMovement>().enabled = false;
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Invoke(nameof(ReoloadLevel), 1.3f);
+        dead = true;
+    }
+
+    void ReoloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
